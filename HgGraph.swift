83fb66d56ae356318fae265f4a990ece0853262e
sntwo <joshuaknapp@me.com>
2016-03-06 13:48:05 -0800

    //adding HgGraph and shortest path search

--- HgGraph.swift
+++ HgGraph.swift
@@ -9,10 +9,14 @@
 import Foundation
 import simd
 
-class HgGraphVertex:Hashable{
+class HgGraphVertex:Hashable, CustomStringConvertible{
     
     var position = float2(0,0)
     var neighbors = Set<HgGraphEdge>()
+    var visited = false
+    var path:HgGraphPath?
+    
+    var description:String { get { return "\(position.x) \(position.y)" } }
     
     init(position:float2){
         self.position = position
@@ -66,6 +70,11 @@ func ==(lhs: HgGraphEdge, rhs: HgGraphEdge) -> Bool {
     return lhs.hashValue == rhs.hashValue
 }
 
+enum djError:ErrorType {
+    case notFound
+    case unknown
+}
+
 class HgGraph {
     
     private var canvas = Set<HgGraphVertex>()
@@ -76,5 +85,82 @@ class HgGraph {
         canvas.insert(newVertex)
         return newVertex
     }
+    
+    ///find Dijkstra's shortest path
+    //reference https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+    func dijkstra(from: HgGraphVertex, to: HgGraphVertex) throws -> HgGraphPath {
+    
+        var frontier = Set<HgGraphVertex>()
+        for n in canvas {
+            n.visited = false
+            n.path = nil
+        }
+        
+        let sourcePath = HgGraphPath(node: from)
+        sourcePath.distance = 0
+        from.path = sourcePath
+        
+        for edge in from.neighbors {
+            let path = HgGraphPath(node: edge.toNeighbor)
+            path.distance = edge.weight
+            path.previous = sourcePath
+            edge.toNeighbor.path = path
+            frontier.insert(edge.toNeighbor)
+        }
+        
+        while !frontier.isEmpty {
+            for node in frontier {
+                for edge in node.neighbors {
+                    let dist = edge.weight + node.path!.distance
+                    if let p = edge.toNeighbor.path {
+                        if p.distance > dist {
+                            let newPath = HgGraphPath(node: edge.toNeighbor)
+                            newPath.distance = dist
+                            newPath.previous = node.path!
+                            edge.toNeighbor.path = newPath
+                        }
+                    } else {
+                        let newPath = HgGraphPath(node: edge.toNeighbor)
+                        newPath.distance = dist
+                        newPath.previous = node.path!
+                        edge.toNeighbor.path = newPath
+                        frontier.insert(edge.toNeighbor)
+                    }
+                }
+                frontier.remove(node)
+            }
+        }
+        if let p = to.path {
+            return p
+        } else {
+            throw djError.notFound
+        }
+    }
+    
+}
+
+//conforming HgGraphEdge to Equatable
+func ==(lhs: HgGraphPath, rhs: HgGraphPath) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
+class HgGraphPath:Hashable {
+    
+    ///the distance of the path through nodes
+    var distance: Float = FLT_MAX
+    var node:HgGraphVertex
+    var previous: HgGraphPath?
+    
+    init(node:HgGraphVertex){
+        self.node = node
+    }
+
+    ///conforming to Hashable
+    var hashValue : Int {
+        get {
+            return "\(previous?.hashValue)".hashValue
+        }
+    }
+    
     
 }
